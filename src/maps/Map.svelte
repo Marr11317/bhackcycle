cb19c3c9c8b462327442c5afa78ce3e49141e2a0
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-02-12 16:12:59 -0500

    Refresh map after 300ms

--- src/maps/Map.svelte
+++ src/maps/Map.svelte
@@ -1,6 +1,7 @@
 <script>
 
     import L from "leaflet";
+    import {onMount} from "svelte";
     //import { Geolocation } from '@capacitor/geolocation';
 
     let map;
@@ -11,14 +12,13 @@
         let m = L.map(container, {preferCanvas: true });
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 20,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
             accessToken: 'pk.eyJ1IjoicmVkNWg0ZDB3IiwiYSI6ImNrems4bzhzMzRrcG4yeHByYTgwN2draHcifQ.356a0A2cfoy3zrhq-IW_rA'
         }).addTo(m);
         m.setView(initialView, 14);
-        m.invalidateSize();
         return m;
     }
 
@@ -49,6 +49,12 @@
     function resizeMap() {
 	    if(map) { map.invalidateSize(); }
     }
+
+    onMount(() => {
+            setTimeout(() => resizeMap(), 300);
+        }
+       
+    )
 
 </script>
 
