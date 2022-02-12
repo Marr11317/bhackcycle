76ff1b2692158cbce9d8bb6d6fd4f86ee3d336a2
Remi Marche <remimarche@gmail.com>
2022-02-11 20:44:42 -0500

    initial working version

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,21 @@
-<script lang="ts">
-	let name: string = 'world';
-</script>
+<ion-app>
+	<ion-tabs>
+		<ion-tab tab="home">
+			<h1>Home Content</h1>
+		</ion-tab>
+		<ion-tab tab="settings">
+			<h1>Settings Content</h1>
+		</ion-tab>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
-
-		@media (min-width: 640px) {
-			max-width: none;
-		}
-	}
-</style>
+		<ion-tab-bar slot="bottom">
+			<ion-tab-button tab="home">
+				<ion-label>Home</ion-label>
+				<ion-icon name="home" />
+			</ion-tab-button>
+			<ion-tab-button tab="settings">
+				<ion-label>Settings</ion-label>
+				<ion-icon name="settings" />
+			</ion-tab-button>
+		</ion-tab-bar>
+	</ion-tabs>
+</ion-app>
