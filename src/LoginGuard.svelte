dff8f12b04ffcdc6faa9185e69096009a6026828
Remi Marche <remimarche@gmail.com>
2022-02-12 15:49:56 -0500

    fis signin page

--- src/LoginGuard.svelte
+++ src/LoginGuard.svelte
@@ -1,38 +1,20 @@
 <script lang="ts">
-    import SignupScreen from "./SignupScreen.svelte";
     import LoginScreen from "./LoginScreen.svelte";
     import App from "./App.svelte";
 
-    import auth from './auth';
+    import auth from "./auth";
 
     let logged = auth.isLoggedIn();
     let signup = false;
 </script>
 
 <ion-app>
-	{#if !logged}
-    <ion-header translucent>
-        <ion-toolbar>
-            <ion-title>{signup ? "Sign up" : "Login"}</ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-list lines="full">
-        <ion-item>
-            <ion-input placeholder="placeholder" type="text" />
-        </ion-item>
-        <ion-item>
-            <ion-input placeholder="password" type="password" />
-        </ion-item>
-        {#if signup}
-        {/if}
-        <ion-button expand="block">Signup</ion-button>
-        </ion-list>
-        <ion-button  on:click="{() => signup = true }">{signup ? "Login instead" : "Sign up instead"}</ion-button>
+    {#if !logged}
+        <LoginScreen bind:signup/>
     {:else}
-    <App/>
+        <App />
     {/if}
 </ion-app>
 
 <style>
-
 </style>
--- src/LoginScreen.svelte
+++ src/LoginScreen.svelte
@@ -1,19 +1,26 @@
-<script>
-
+<script lang="ts">
+    export let signup = false;
 </script>
-<ion-content fullscreen>
-    <ion-header translucent>
-        <ion-toolbar>
-            <ion-title>Login</ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-list lines="full">
+
+<ion-header translucent>
+    <ion-toolbar>
+        <ion-title>{signup ? "Sign up" : "Login"}</ion-title>
+    </ion-toolbar>
+</ion-header>
+<ion-list lines="full">
+    <ion-item>
+        <ion-input placeholder="email" type="text" />
+    </ion-item>
+    <ion-item>
+        <ion-input placeholder="password" type="password" />
+    </ion-item>
+    {#if signup}
         <ion-item>
-            <ion-input placeholder="placeholder" type="text" />
+            <ion-input placeholder="confirm password" type="password" />
         </ion-item>
-        <ion-item>
-            <ion-input placeholder="password" type="password" />
-        </ion-item>
-        <ion-button expand="block">Login</ion-button>
-    </ion-list>
-</ion-content>
+    {/if}
+    <ion-button expand="block">{signup ? "Sign up" : "Login"}</ion-button>
+</ion-list>
+<ion-button on:click={() => (signup = !signup)}>
+    {signup ? "Login instead" : "Sign up instead"}
+</ion-button>
