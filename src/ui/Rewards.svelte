5992bc61b3637bce11c5677941773a35569c6f86
Remi Marche <remimarche@gmail.com>
2022-02-13 01:52:53 -0500

    remove forgotten player points

--- src/ui/Rewards.svelte
+++ src/ui/Rewards.svelte
@@ -1,27 +1,23 @@
 <script lang="ts">
   import { allRewards, loadedUser } from "./../app-state";
   import RewardCard from "./Rewards/RewardCard.svelte";
-
-  let playerPoints: number;
-  loadedUser.subscribe((loadedUser) =>{
-      if(loadedUser !== null){
-          playerPoints = loadedUser.credits;
-      }
-    })
 </script>
 
 <ion-header translucent>
   <ion-toolbar>
     <ion-title>Rewards</ion-title>
-    <ion-subtitle>You have {$loadedUser?.credits} points to spend.</ion-subtitle
-    >
   </ion-toolbar>
 </ion-header>
 
 <ion-content>
-  {#if $allRewards}
-    {#each $allRewards as reward}
-      <RewardCard {reward} />
-    {/each}
-  {/if}
+  <ion-list>
+    <ion-list-header>
+      You have {$loadedUser?.credits} points to spend.
+    </ion-list-header>
+    {#if $allRewards}
+      {#each $allRewards as reward}
+        <RewardCard {reward} />
+      {/each}
+    {/if}
+  </ion-list>
 </ion-content>
