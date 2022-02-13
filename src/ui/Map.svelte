191d144f87b247c43c2c80aa7d204c6e0bbcc095
Remi Marche <remimarche@gmail.com>
2022-02-12 22:37:15 -0500

    typescript rewards

--- src/ui/Map.svelte
+++ src/ui/Map.svelte
@@ -176,7 +176,7 @@
 <svelte:head>
   <link
     rel="stylesheet"
-    href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+    href="../../node_modules/dist/leaflet.css"
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     crossorigin=""
   />
