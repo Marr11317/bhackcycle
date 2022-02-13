65ecde85192b27e428d62e6829bdf2f9f00100b1
Remi Marche <remimarche@gmail.com>
2022-02-12 19:00:02 -0500

    add Modal module

---
+++ src/ui/Dashboard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import PopoverExtra from "./PopoverExtra.svelte";
+    import { IonicShowPopover } from "./IonicControllers";
+
+    function showPopover(event: Event) {
+        console.log("here");
+        IonicShowPopover(event, "popover-extra", PopoverExtra, {
+            firstName: "Douglas",
+            lastName: "Adams",
+            middleInitial: "N",
+        }).then(console.log);
+    };
+</script>
+
+<ion-header translucent>
+    <ion-toolbar>
+        <ion-buttons slot="start">
+            <ion-menu-button auto-hide="false" />
+        </ion-buttons>
+        <ion-buttons slot="primary">
+            <ion-button on:click={showPopover}>
+                <ion-icon
+                    slot="icon-only"
+                    ios="ellipsis-horizontal"
+                    md="ellipsis-vertical"
+                />
+            </ion-button>
+        </ion-buttons>
+
+        <ion-title>Dashboard</ion-title>
+    </ion-toolbar>
+</ion-header>
+
+<h1 class="ma">Dashboard Content</h1>
---
+++ src/ui/PopoverExtra.svelte
@@ -0,0 +1,51 @@
+<script>
+    let overlayElement = document.querySelector("ion-popover");
+    const closeOverlay = (data) => {
+        overlayElement.dismiss(data);
+    };
+</script>
+
+<ion-list>
+    <ion-list-header>Ionic</ion-list-header>
+    <ion-item
+        button
+        on:click={() => {
+            closeOverlay("Learn Ionic");
+        }}
+    >
+        Learn Ionic
+    </ion-item>
+    <ion-item
+        button
+        on:click={() => {
+            closeOverlay("Documentation");
+        }}
+    >
+        Documentation
+    </ion-item>
+    <ion-item
+        button
+        on:click={() => {
+            closeOverlay("Showcase");
+        }}
+    >
+        Showcase
+    </ion-item>
+    <ion-item
+        button
+        on:click={() => {
+            closeOverlay("Github");
+        }}
+    >
+        GitHub Repo
+    </ion-item>
+</ion-list>
+
+<ion-button
+    expand="block"
+    on:click={() => {
+        closeOverlay(undefined);
+    }}
+>
+    Close
+</ion-button>
