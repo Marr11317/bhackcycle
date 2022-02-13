cb328720e47c0963e137496caf0fc6a13c665345
Remi Marche <remimarche@gmail.com>
2022-02-12 22:36:53 -0500

    typescript rewards

--- src/ui/Rewards/RewardCard.svelte
+++ src/ui/Rewards/RewardCard.svelte
@@ -1,68 +1,72 @@
 <script lang="ts">
-  import { loadedUser } from "../../app-state";
+    import { loadedUser } from "../../app-state";
 
-  export let reward: Reward;
-  export let isRedeemed = false;
-  const tryToRedeem = () => {
-    if (
-      isRedeemed ||
-      reward.remainingUnits === 0 ||
-      $loadedUser!.credits < reward.price
-    ) {
-      return;
+    export let reward: Reward;
+    export let isRedeemed = false;
+
+    function tryToRedeem() {
+        if (
+            !isRedeemed ||
+            !reward.remainingUnits ||
+            $loadedUser!.credits < reward.price
+        )
+            return;
+
+        isRedeemed = true;
+        if ($loadedUser) {
+            $loadedUser.credits -= reward.price;
+            reward.remainingUnits--;
+        }
     }
 
     isRedeemed = true;
-    loadedUser.update(user => {
-      if (!user) {
-        return null;
-      }
-      user.credits -= reward.price;
-      return user;
+    loadedUser.update((user) => {
+        if (!user) {
+            return null;
+        }
+        user.credits -= reward.price;
+        return user;
     });
-
-    if (reward.remainingUnits) {
-      reward.remainingUnits--;
-    }
-  };
 </script>
 
 <ion-card
-  on:click={tryToRedeem}
-  class={isRedeemed ? "redeemed" : "notRedeemed"}
+    on:click={tryToRedeem}
+    class={isRedeemed ? "redeemed" : "notRedeemed"}
 >
-  <ion-card-header>
-    <ion-card-title>{reward.name}</ion-card-title>
-    <ion-card-subtitle class="sub">
-      {reward.description}
-    </ion-card-subtitle>
-  </ion-card-header>
+    <ion-card-header>
+        <ion-card-title>{reward.name}</ion-card-title>
+        <ion-card-subtitle class="sub">
+            {reward.description}
+        </ion-card-subtitle>
+    </ion-card-header>
 
-  <ion-card-content class="content">
-    Cost: {reward.price} credits.
-    <br/>
-    {reward.expiration ? `Expires on ${reward.expiration.toDateString()}` : ''}
-  </ion-card-content>
+    <ion-card-content class="content">
+        Cost: {reward.price} credits.
+        <br />
+        {reward.expiration
+            ? `Expires on ${reward.expiration.toDateString()}`
+            : ""}
+    </ion-card-content>
 </ion-card>
 
 <style>
-  ion-card {
-    color: aliceblue;
-    visibility: visible;
-  }
-  .redeemed {
-    background-color: beige;
-  }
-  .notRedeemed {
-    background-color: #c3e4f1;
-  }
+    ion-card {
+        color: aliceblue;
+        visibility: visible;
+    }
+    .redeemed {
+        background-color: beige;
+    }
+    .notRedeemed {
+        background-color: #c3e4f1;
+    }
 
-  .sub {
-    font-size: 1rem;
-    color: gray;
-  }
+    .sub {
+        font-size: 1rem;
+        color: gray;
+    }
 
-  .content {
-    color: gray
-  }
+    .content {
+        color: gray;
+    }
 </style>
