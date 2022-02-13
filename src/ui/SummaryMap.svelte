5e764a49e2f8d8db4949455b1939b8149e291061
Totom3 <devtomermoran@gmail.com>
2022-02-13 08:37:39 -0500

    fix

--- src/ui/SummaryMap.svelte
+++ src/ui/SummaryMap.svelte
@@ -1,111 +1,107 @@
 <script lang="ts">
-
     export let trip: Trip;
 
     import "../../node_modules/leaflet/dist/leaflet.css";
-  
+
     import L from "leaflet";
     import { onMount } from "svelte";
-    import {
-      averagePosition
-    } from "../app-state";
-  
+    import { averagePosition } from "../app-state";
+
     let carte: L.Map | null = null;
     let trajetActuel: L.Polyline | null = null;
     let circleStartMarker: L.CircleMarker | null = null;
     let circleEndMarker: L.CircleMarker | null = null;
-  
+
     const createMap = (container: HTMLElement) => {
-      carte = L.map(container, { preferCanvas: true });
-      L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 20,
-          minZoom: 1,
-          id: "mapbox/streets-v11",
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken:
-            "pk.eyJ1IjoicmVkNWg0ZDB3IiwiYSI6ImNrems4bzhzMzRrcG4yeHByYTgwN2draHcifQ.356a0A2cfoy3zrhq-IW_rA",
-        }
-      ).addTo(carte);
-      carte.setView([45.504755, -73.612572], 14); // Montreal
-      return {
-        destroy: () => {
-          carte?.remove();
-          carte = null;
-        },
-      };
+        carte = L.map(container, { preferCanvas: true });
+        L.tileLayer(
+            "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+            {
+                attribution:
+                    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 20,
+                minZoom: 1,
+                id: "mapbox/streets-v11",
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken:
+                    "pk.eyJ1IjoicmVkNWg0ZDB3IiwiYSI6ImNrems4bzhzMzRrcG4yeHByYTgwN2draHcifQ.356a0A2cfoy3zrhq-IW_rA",
+            }
+        ).addTo(carte);
+        carte.setView([45.504755, -73.612572], 14); // Montreal
+        return {
+            destroy: () => {
+                carte?.remove();
+                carte = null;
+            },
+        };
     };
-  
+
     const resizeMap = () => carte?.invalidateSize();
 
     const drawTrip = () => {
-      if (!carte) {
-        return;
-      }
-  
-      const pointList = trip.geopoints
-        .sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1))
-        .map<[number, number]>((x) => [
-          x.location.latitude,
-          x.location.longitude,
-        ]);
-  
-      circleStartMarker = L.circleMarker(pointList[0], {
-        color: "red",
-        weight: 2,
-        opacity: 0.7,
-      });
-      circleEndMarker = L.circleMarker(pointList[pointList.length - 1], {
-        color: "red",
-        weight: 2,
-        opacity: 0.7,
-      });
-      trajetActuel = L.polyline(pointList, {
-        color: "red",
-        weight: 3,
-        opacity: 0.5,
-        smoothFactor: 1,
-      });
-  
-      circleStartMarker.addTo(carte);
-      circleEndMarker.addTo(carte);
-      trajetActuel.addTo(carte);
-    };
-  
-    onMount(() => {
-      setTimeout(async () => {
-        if(trip) {
-            console.log(trip)
-            var center = averagePosition(trip)
-            carte?.setView([center.latitude, center.longitude]);
-            drawTrip();
+        if (!carte) {
+            return;
         }
-        resizeMap();
-      }, 300);
-    });
-  </script>
-  
-  <svelte:window on:resize={resizeMap} />
 
-  <div class="map" style="height: 100%; width: 100%;" use:createMap />
-  
-  <style>
+        const pointList = trip.geopoints
+            .sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1))
+            .map<[number, number]>((x) => [
+                x.location.latitude,
+                x.location.longitude,
+            ]);
+
+        circleStartMarker = L.circleMarker(pointList[0], {
+            color: "red",
+            weight: 2,
+            opacity: 0.7,
+        });
+        circleEndMarker = L.circleMarker(pointList[pointList.length - 1], {
+            color: "red",
+            weight: 2,
+            opacity: 0.7,
+        });
+        trajetActuel = L.polyline(pointList, {
+            color: "red",
+            weight: 3,
+            opacity: 0.5,
+            smoothFactor: 1,
+        });
+
+        circleStartMarker.addTo(carte);
+        circleEndMarker.addTo(carte);
+        trajetActuel.addTo(carte);
+    };
+
+    onMount(() => {
+        setTimeout(async () => {
+            if (trip) {
+                console.log(trip);
+                var center = averagePosition(trip);
+                carte?.setView([center.latitude, center.longitude]);
+                drawTrip();
+            }
+            resizeMap();
+        }, 1000);
+    });
+</script>
+
+<svelte:window on:resize={resizeMap} />
+
+<div class="map" style="height: 100%; width: 100%;" use:createMap />
+
+<style>
     .map :global(.marker-text) {
-      width: 100%;
-      text-align: center;
-      font-weight: 600;
-      background-color: #444;
-      color: #eee;
-      border-radius: 0.5rem;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        background-color: #444;
+        color: #eee;
+        border-radius: 0.5rem;
     }
-  
+
     .map :global(.map-marker) {
-      width: 30px;
-      transform: translateX(-50%) translateY(-25%);
+        width: 30px;
+        transform: translateX(-50%) translateY(-25%);
     }
-  </style>
-  
+</style>
--- src/ui/Trip.svelte
+++ src/ui/Trip.svelte
@@ -1,33 +1,42 @@
 <script lang="ts">
-    export let trip: Trip;
-    export let editable = false;
-    let nodeRef: HTMLElement;
+  export let trip: Trip;
+  export let editable = false;
+  let nodeRef: HTMLElement;
 
-    import { computeTripDistance } from "../app-state";
-    import { IonicShowModal } from "./IonicControllers";
-    import SummaryMap from "./SummaryMap.svelte";
-    import database from './../database';
-    function deleteTrip() {
-        database.deleteTrip(trip.id);
-    }
-    import { iconNameForTransportType } from "./utilities";
+  import { computeTripDistance } from "../app-state";
+  import { IonicShowModal } from "./IonicControllers";
+  import SummaryMap from "./SummaryMap.svelte";
+  import database from "./../database";
+  function deleteTrip() {
+    database.deleteTrip(trip.id);
+  }
+  import { iconNameForTransportType } from "./utilities";
 
-    function viewTrip(event: Event) {
-        IonicShowModal("summary-map", SummaryMap, {'trip': trip});
-    }
+  let showTrip = false;
+  function viewTrip(event: Event) {
+    showTrip = true;
+    //IonicShowModal("summary-map", SummaryMap, {'trip': trip});
+  }
 </script>
 
-
 <ion-item bind:this={nodeRef}>
-    <ion-icon
-        name={iconNameForTransportType(trip.transportType)}
-        slot="start"
-    />
-    <ion-label on:click={viewTrip}>
-        {computeTripDistance(trip.geopoints).toLocaleString(undefined, {maximumFractionDigits:0}) + " m"}
-    </ion-label>
+  <ion-icon name={iconNameForTransportType(trip.transportType)} slot="start" />
+  <ion-label on:click={viewTrip}>
+    {computeTripDistance(trip.geopoints).toLocaleString(undefined, {
+      maximumFractionDigits: 0,
+    }) + " m"}
+  </ion-label>
 
-    <ion-button hidden={!editable} on:click={() => {
-        deleteTrip();
-        nodeRef?.parentNode?.removeChild(nodeRef);}}>Delete</ion-button>
+  <ion-button
+    hidden={!editable}
+    on:click={() => {
+      deleteTrip();
+      nodeRef?.parentNode?.removeChild(nodeRef);
+    }}>Delete</ion-button
+  >
 </ion-item>
+{#if showTrip}
+  <ion-modal is-open>
+    <SummaryMap {trip} />
+  </ion-modal>
+{/if}
